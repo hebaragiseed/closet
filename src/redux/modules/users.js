d8485bc6327840ab4d77481adeb6cef0342f2301
hebaragiseed <hebaragiseed@gmail.com>
2018-01-18 06:54:04 +0900

    user Reducer

---
+++ src/redux/modules/users.js
@@ -0,0 +1,24 @@
+//import 
+
+//actions
+
+//actions creator
+
+//initial state
+const initialState = {
+  isLoggedIn: false
+}
+//reducer
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+//reducer functions
+
+//exports
+
+//reducer export
+export default reducer;
