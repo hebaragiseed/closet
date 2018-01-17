c828b2a10317a49edb61a3fe896ccc11b6936f84
hebaragiseed <hebaragiseed@gmail.com>
2018-01-18 08:28:17 +0900

    .env파일추가_ 파일들 src부터 불러오도록 설정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/configureStore';
-import './index.css';
-import App from './App';
+import store from 'redux/configureStore';
+import 'index.css';
+import App from 'App';
 
 //리액트 앱과 리듀서 연결
 //console.log(store.getState());
