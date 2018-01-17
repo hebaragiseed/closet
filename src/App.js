5eb7c1df9d758f6aa392cf0f2145462637f7d0f2
hebaragiseed <hebaragiseed@gmail.com>
2018-01-18 07:50:34 +0900

    configureStore파일 만들기

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <header className={styles.App__header}>
-          <img src="" className={styles.App__logo} alt="logo" />
-          <h1 className={styles.App__title}>Welcome to React</h1>
-        </header>
-        <p className={styles.App__intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
