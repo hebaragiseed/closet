f8632c8dbcea7de6e04de96bb6af87b2c4e74ed8
hebaragiseed <hebaragiseed@gmail.com>
2018-01-18 08:14:30 +0900

    리액트앱과 리듀서연결

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './index.css';
 import App from './App';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+//리액트 앱과 리듀서 연결
+//console.log(store.getState());
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+   document.getElementById('root'));
