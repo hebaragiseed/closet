5448c6741964023b6c49f90020a60d0629ab41f7
hebaragiseed <hebaragiseed@gmail.com>
2018-01-18 18:38:53 +0900

    리액트라우터와 리듀서 함께 작동하도록 하기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from 'redux/configureStore';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from 'redux/configureStore';
 import 'index.css';
 import App from 'App';
 
-store.dispatch({type: "BULL"})
+//store.dispatch({type: "BULL"})
 
 //리액트 앱과 리듀서 연결
 //console.log(store.getState());
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+  {/*라우터에게 history오브젝트주기, 그래서 라우터의 히스토리오브젝트와 나의 미들웨어 히스토리오브젝트와 같은 오브젝트 같게됨*/}
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
    document.getElementById('root')
   );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 //리덕스 스토어로 우리가 원할때 액션을 보내줌
 import thunk from 'redux-thunk';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import  createHistory from 'history/createBrowserHistory';
 import users from 'redux/modules/users';
 
-const middlewares = [thunk];
 //process의 전체 정보를 가짐
 const env = process.env.NODE_ENV;
+
+const history = createHistory();
+//routerMiddleware는 history랑 싱크되야함
+//history오브젝트를 미들웨어에게 주기
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === 'development') {
   const {logger} = require('redux-logger');
@@ -13,9 +19,11 @@ if (env === 'development') {
 }
 
 const reducer = combineReducers({
-  users
+  users,
+  routing: routerReducer
 });
 
 let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
-
+//router는 history 오브젝트 필요함
+export { history };
 export default store();
