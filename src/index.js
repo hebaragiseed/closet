81223021a62d1649e3d8551e86a596c069c7f1ae
hebaragiseed <hebaragiseed@gmail.com>
2018-01-18 10:00:20 +0900

    미들웨어 thunk, logger추가

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import store from 'redux/configureStore';
 import 'index.css';
 import App from 'App';
 
+store.dispatch({type: "BULL"})
+
 //리액트 앱과 리듀서 연결
 //console.log(store.getState());
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-   document.getElementById('root'));
+   document.getElementById('root')
+  );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,21 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+//리덕스 스토어로 우리가 원할때 액션을 보내줌
+import thunk from 'redux-thunk';
 import users from 'redux/modules/users';
+
+const middlewares = [thunk];
+//process의 전체 정보를 가짐
+const env = process.env.NODE_ENV;
+
+if (env === 'development') {
+  const {logger} = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const reducer = combineReducers({
   users
 });
 
-let store = initialState => createStore(reducer);
+let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
