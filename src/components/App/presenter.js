87a0d23790dc0a35ab2dd9247adf9071209625df
hebaragiseed <hebaragiseed@gmail.com>
2018-01-19 19:49:58 +0900

    App 프리젠터 라우터

--- src/components/App/presenter.js
+++ src/components/App/presenter.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import styles from './styles.scss';
 import Footer from 'components/Footer'
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = (props) => [
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  // 리액트는 array를 리턴 할때마다  key로 숫자를 필요
+  <Footer key={3} />  
+];
+
+const PrivateRoutes = () => (
+  <Switch>
+    <Route exact path="/" render={() => "feed" } />
+    <Route exact path="/explore" render={() => "explore" } />
+  </Switch>
+);
+
+const PublicRoutes = () => (
+  <Switch>
+    <Route exact path="/" render={() => "login"} />
+    <Route exact path="/forgot" render={() => "password"} />
+  </Switch>
+); 
 
 export default App;
