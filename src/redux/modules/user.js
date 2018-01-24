92016e1bba35de14cf329a7effadb519b4ce8954
hebaragiseed <hebaragiseed@gmail.com>
2018-01-24 18:01:55 +0900

    회원가입,로그인 완료

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,102 +1,104 @@
 //import 
-import { auth, db, googleProvider } from 'firebase/client';
+import { auth, googleProvider } from 'firebase/client';
 
 //actions
-
+const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCES';
 
 //actions creator
+function loginUserSuccess(name, uid) {
+  return {
+    type: LOGIN_USER_SUCCESS,
+    name,
+    uid
+  };
+}
 
 //API actions
+//구글로 로그인할 때
 function googleLogin() {
   return (dispatch) => {
     auth.signInWithPopup(googleProvider) 
-    .then(result => {
-      const user = auth.currentUser     
-      if (user != null) {
-        user.providerData.forEach(function (profile) {
-          console.log("Sign-in provider: "+profile.providerId);
-          console.log("  Provider-specific UID: "+profile.uid);
-          console.log("  Name: "+profile.displayName);
-          console.log("  Email: "+profile.email);
-          console.log("  Photo URL: "+profile.photoURL);
-        });
-        auth.onAuthStateChanged(function(userState) {
-          if (userState) {
-            // User is signed in.
-            console.log('userlogin')
-          } else {
-            // No user is signed in.
-            console.log('userlogin')
-          }
-        });
-      }
-  })
-  .catch(err => {console.log(err.message)})
-  }
-  
+    .then(() => {
+      const user = auth.currentUser;
+      user.providerData.forEach(function (profile) {
+        const name = profile.displayName;
+        const uid = profile.uid;              
+      //const email = profile.email;
+        dispatch(loginUserSuccess(name, uid))            
+      })
+    })
+    .catch(err => {console.log(err.message)})
+  }  
 }
 
 //이메일, 비밀번호로 로그인할 때 
 function emailLogin(email, password) {
   return (dispatch => {
     auth.signInWithEmailAndPassword(email, password)
-    .then(result => {
-      auth.onAuthStateChanged(function(user) {
-        if (user) {
-          // User is signed in.
-            const user = auth.currentUser;
-            let name, email, photoUrl, uid, emailVerified;
-      
-          if (user != null) {
-          name = user.displayName;
-          email = user.email;
-          photoUrl = user.photoURL;
-          emailVerified = user.emailVerified;
-          uid = user.uid; 
-          console.log(name, email, uid)
-          }
-
-        } else {
-          // No user is signed in.
-        }
-      });
-      
-    })
-    
-    .catch(err => err.message)
-  })
-  // auth.onAuthStateChanged(firebaseUser => {
-  //   if(firebaseUser) {
-  //     console.log(firebaseUser)
-  //   } else  {
-  //     console.log('not logged in');
-  //   }
-  // });
-  
+    .then(() => {
+      const user = auth.currentUser;
+      const name = user.displayName;
+      const uid = user.uid; 
+      //const email = user.email;
+      dispatch(loginUserSuccess(name, uid,))
+      }
+    )  
+    .catch(err => console.log(err.message))
+  });
 }
+
 //회원가입할 때 
 function createAccount(name, email, password) {
-  return (dispach) => {
+  return (dispach => {
     auth.createUserWithEmailAndPassword(email, password)
-    .then(console.log('가입왼료'))
-    .catch(err => console.log(err.message))
-  } 
-    
+    .then(() => {
+      console.log('가입왼료')
+      alert('가입 되었습니다')
+      var user = auth.currentUser;
+      user.updateProfile({
+      displayName: name
+      })
+      .then(function() {
+      console.log("이름저장성공")
+      })
+      .catch(function(error) {
+        var user = auth.currentUser;
+        user.delete()
+        alert('다시 회원가입을 작성해주세요')
+        
+      });
+    })
+    .catch(err => {
+      console.log(err.message);
+      alert('가입이 되지 않았습니다. 다시 회원가입해 주세요')
+      }
+    )
+  });    
 }
 
 //initial state
 const initialState = {
   isLoggedIn: false
-}
+};
 //reducer
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case LOGIN_USER_SUCCESS:
+      return applySetUsers(state, action)
     default:
       return state
   }
 }
 
 //reducer functions
+function applySetUsers(state, action) {
+  return {
+    ...state,
+    isLoggedIn: true,
+    name: action.name,
+    uid: action.uid
+  }
+}
 
 //exports
 const actionCreators = {
